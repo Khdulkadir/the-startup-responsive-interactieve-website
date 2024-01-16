603b0a1d0e7d8c7198bff3ce2c33ce68acc9c0dd
Khdulkadir <khdulkadir@gmail.com>
2024-01-16 18:57:07 +0100

    .

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector(".menu");
+const menuToggle = document.querySelector(".menu-toggle");
+const menuClose = document.querySelector(".fa-xmark");
+const body = document.body;
+
+menuToggle.addEventListener("click", menuSlide);
+menuClose.addEventListener("click", menuSlide);
+
+function menuSlide() {
+  menu.classList.toggle("show");
+  body.classList.toggle("stop-scroll");
+}
