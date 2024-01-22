503743abf7c76eed36241fffd85e2b778a6602f7
Khdulkadir <khdulkadir@gmail.com>
2024-01-22 11:05:54 +0100

    .

--- scripts/script.js
+++ scripts/script.js
@@ -5,8 +5,17 @@ const body = document.body;
 
 menuToggle.addEventListener("click", menuSlide);
 menuClose.addEventListener("click", menuSlide);
+body.addEventListener("click", closeMenuOutsideClick);
 
 function menuSlide() {
   menu.classList.toggle("show");
   body.classList.toggle("stop-scroll");
+}
+
+function closeMenuOutsideClick(event) {
+  // Check if the clicked element is not inside the menu
+  if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
+    menu.classList.remove("show");
+    body.classList.remove("stop-scroll");
+  }
 }
